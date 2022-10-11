b8dedc7ddff0c8e0a6544bd3eba033633dc98a9d
maciej790 <maciejmw126@gmail.com>
2022-10-11 17:22:31 +0200

    base setup

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Welcome from "./Routes/Welcome/Welcome";
+import Home from "./Routes/Home/Home";
+import Recipe from "./Routes/Recipe/Recipe";
+import Create from "./Routes/Create/Create";
+import "./styles/main.scss";
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/recipe" element={<Recipe />} />
+        <Route path="/create" element={<Create />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
